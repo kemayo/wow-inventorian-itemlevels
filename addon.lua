82c0ddb7f5861b1a0b6a5db843335950801bdd0f
David Lynch <kemayo@gmail.com>
2017-04-13 01:00:35 -0500

    Initial version

---
+++ addon.lua
@@ -0,0 +1,39 @@
+local myname, ns = ...
+
+local inv = LibStub("AceAddon-3.0"):GetAddon("Inventorian")
+local original_WrapItemButton = inv.Item.WrapItemButton
+inv.Item.WrapItemButton = function(...)
+	local item = original_WrapItemButton(...)
+
+	item.ItemLevel = item:CreateFontString('$parentItemLevel', 'ARTWORK')
+	item.ItemLevel:SetPoint('TOPRIGHT', -2, -2)
+	item.ItemLevel:SetFontObject(NumberFontNormal)
+	item.ItemLevel:SetJustifyH('RIGHT')
+
+	return item
+end
+
+local original_Update = inv.Item.prototype.Update
+inv.Item.prototype.Update = function(self, ...)
+	if self:IsVisible() then
+		local icon, count, locked, quality, readable, lootable, link, noValue, itemID = self:GetInfo()
+		self.ItemLevel:Hide()
+		if itemID and link then
+			local _, _, _, _, _, itemClass, itemSubClass = GetItemInfoInstant(itemID)
+			if
+				quality >= LE_ITEM_QUALITY_UNCOMMON and (
+					itemClass == LE_ITEM_CLASS_WEAPON or
+					itemClass == LE_ITEM_CLASS_ARMOR or
+					(itemClass == LE_ITEM_CLASS_GEM and itemSubClass == 11) -- artifact relic
+				)
+			then
+				local r, g, b, hex = GetItemQualityColor(quality)
+				local itemLevel = select(4, GetItemInfo(link))
+				self.ItemLevel:SetFormattedText('|c%s%s|r', hex, itemLevel)
+				self.ItemLevel:Show()
+			end
+		end
+	end
+
+	return original_Update(self, ...)
+end
